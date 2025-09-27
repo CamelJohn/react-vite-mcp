0e7bca94475111547279060352d9e5fff7b46357
CamelJon <jonathan23986@gmail.com>
2025-09-27 11:18:51 +0300

    cleanup + add context + barrel files

---
+++ templates/context/index.ts
@@ -0,0 +1,4 @@
+export * from './context';
+export * from './provider.tsx';
+export * from './usePlaceholder';
+export * from './types';
---
+++ templates/feature/index.ts
@@ -0,0 +1,5 @@
+export * from './action';
+export * from './loader';
+export * from './page';
+export * from './error.boundary';
+export * from './middleware';
